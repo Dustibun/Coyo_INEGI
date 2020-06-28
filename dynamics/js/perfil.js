29f74bc8b3ec110ab722c28f7441cc025228c28c
Oviedo404 <oviedovaleria@comunidad.unam.mx>
2020-06-28 09:27:34 -0500

    FEAT: Versión final, se arregló problema con GitHub

---
+++ dynamics/js/perfil.js
@@ -0,0 +1,177 @@
+getSession();
+
+window.addEventListener("load", function(event) {
+  console.log("Listo");
+  getUser();
+})
+
+document.getElementById("misencuestas").addEventListener("click",(event)=>{
+  getPoll();
+  console.log("ok111");
+})
+
+document.getElementById("miactividad").addEventListener("click",(event)=>{
+  getActivity();
+  console.log(1);
+})
+
+document.getElementById("foto").addEventListener("click",(event)=>{
+  console.log("Modal");
+  Foto();
+;})
+
+
+
+
+
+let show=document.getElementById("show")
+let container=document.createElement("div");
+
+function getUser() {
+ fetch(`../dynamics/php/user.php?`, {
+   method: 'GET'
+ }).then((response) => {
+
+   return response.json();
+
+ }).then((data) => {
+
+   data.forEach(element => {
+     console.log(element);
+     document.getElementById("nombredelusuario").innerHTML=element.nombre +" "+ element.ap_Pat;
+     document.getElementById("cumple").innerHTML=element.nacimiento;
+     document.getElementById("correo").innerHTML=element.correo;
+     document.getElementById("perfil").setAttribute("src","../statics/media/img/perfil/"+element.foto);
+
+   });
+ })
+}
+
+
+
+
+function getPic() {
+  return "name=" + document.getElementById("file").value;
+}
+
+function getSession() {
+  fetch(`../dynamics/php/sesion.php?`,{
+    method: 'GET'
+  }).then((response)=>{
+
+    return response.json();
+  }).then((data)=>{
+
+    console.log("La sesion es...");
+    if (data!=1) {
+      window.location='inicio.html';
+      console.log("correcta");
+    }
+  })
+}
+
+function getPoll() {
+  fetch(`../dynamics/php/encuestas.php?`, {
+    method: 'GET'
+  }).then((response)=> {
+
+    return response.json();
+
+  }).then((data)=> {
+    show.innerHTML="";
+
+    data.forEach(element =>{
+      console.log(element);
+      container.classList.add("consultaAct")
+      container.classList.remove("consultaEnc");
+      container.innerHTML=`<h2>${element.nombre}`;
+
+      if (element.nombre==undefined) {
+        container.innerHTML=`<h2>No hay no existe`;
+      }
+
+    })
+    show.appendChild(container);
+  })
+}
+function getActivity(){
+  fetch(`../dynamics/php/actividad.php?`, {
+    method: 'GET'
+}).then((response)=> {
+  return response.json();
+
+}).then((data)=> {
+  show.innerHTML="";
+
+  data.forEach(element =>{
+
+    show.innerHTML="";
+
+    data.forEach(element =>{
+      console.log(element);
+      container.classList.remove("consultaAct");
+      container.classList.add("consultaEnc")
+      container.innerHTML=`<h2>${element.nombre}`;
+
+      if (element.nombre==undefined) {
+        container.innerHTML=`<h2>No hay no existe`;
+      }
+
+    })
+    show.appendChild(container);
+  })
+})
+
+}
+
+function getFiles(){
+    var files = document.getElementById("fileText").files;
+    var myArray = [];
+    var file = {};
+
+    console.log(files);
+}
+
+
+function Foto() {
+  show.innerHTML="";
+  container.classList.remove("consultaAct");
+  container.classList.remove("consultaEnc");
+  container.classList.add("foto");
+  container.innerHTML=`
+  <form class="" action="" method="get" id="form" enctype="multipart/form-data">
+    <input type="file" name="file" value="" id="file"><br>
+    <label for="file" id="fileText">Elige una imagen</label>
+    <br><input type="submit" class='button' value='Upload' id='upload'>
+  </form>`;
+
+  show.appendChild(container);
+
+  document.getElementById("form").addEventListener("submit",(event)=>{
+    event.preventDefault();
+
+
+    //getFiles();
+    uploadPic();
+  })
+}
+
+function uploadPic() {
+  pic= document.getElementById("file");
+  var formData = new FormData();
+  formData.append("foto", pic);
+
+  fetch(`../dynamics/php/foto.php?`, {
+    method: 'POST',
+    body:formData
+}).then((response)=>{
+
+  console.log(response);
+  return response.json();
+
+}).then((exito)=>{
+
+  console.log(exito);
+
+})
+}
--- dynamics/js/registro.js
+++ dynamics/js/registro.js
@@ -26,10 +26,10 @@ function creaInput(clase){
     inputCambio.appendChild(textAdmin);
     inputCambio.appendChild(admin);
     uno.setAttribute("name", "noTrabajador");
-    //uno.setAttribute("pattern", "^\d{6}$");
+    uno.setAttribute("pattern", "^\d{6}$");
     titUno.innerText="Número de Trabajador";
     dos.setAttribute("name", "rfc");
-    //dos.setAttribute("pattern", "^[A-Z]{4}\d{2}(0[1-9]|1[012])(0[1-9]|1[0-9]|2[0-9]|3[0-1])[0-9A-Z]{3}$");
+    dos.setAttribute("pattern", "^[A-Z]{4}\d{2}(0[1-9]|1[012])(0[1-9]|1[0-9]|2[0-9]|3[0-1])[0-9A-Z]{3}$");
     titDos.innerText="RFC";
     var boton=document.getElementsByClassName("maestro")[0];
     boton.classList.add("alumno");
@@ -40,10 +40,10 @@ function creaInput(clase){
     titulo.innerText="Alumno";
     tipo.setAttribute("value","alumno");
     uno.setAttribute("name", "noCuenta");
-    //uno.setAttribute("pattern", "^\d{9}$");
+    uno.setAttribute("pattern", "^\d{9}$");
     titUno.innerText="Número de Cuenta";
     dos.setAttribute("name", "curp");
-    //dos.setAttribute("pattern", "^[A-Z]{4}(0[1-9]|[1-9][0-9])(0[1-9]|1[012])(0[1-9]|1[0-9]|2[0-9]|3[01])[MH](AS|BS|CL|CS|DF|GT|HG|MC|MS|NL|PL|QR|SL|TC|TL|YN|NE|BC|CC|CM|CH|DG|GR|JC|MN|NT|OC|QT|SP|SR|TS|VZ|ZS)[A-Z]{3}[A-Z]\w$");
+    dos.setAttribute("pattern", "^[A-Z]{4}(0[1-9]|[1-9][0-9])(0[1-9]|1[012])(0[1-9]|1[0-9]|2[0-9]|3[01])[MH](AS|BS|CL|CS|DF|GT|HG|MC|MS|NL|PL|QR|SL|TC|TL|YN|NE|BC|CC|CM|CH|DG|GR|JC|MN|NT|OC|QT|SP|SR|TS|VZ|ZS)[A-Z]{3}[A-Z]\w$");
     titDos.innerText="CURP";
     var boton=document.getElementsByClassName("alumno")[0];
     boton.classList.add("maestro");
@@ -53,7 +53,7 @@ function creaInput(clase){
     var tres = document.createElement("input");
     tres.setAttribute("name", "fecha");
     tres.setAttribute("type","date");
-    //tres.setAttribute("pattern", "^(19[2-9][0-9]|200[2-3])-(0[1-9]|1[012])-(0[1-9]|1[0-9]|2[0-9]|3[01])$");
+    tres.setAttribute("pattern", "^(19[2-9][0-9]|200[2-3])-(0[1-9]|1[012])-(0[1-9]|1[0-9]|2[0-9]|3[01])$");
     var titTres = document.createElement("h4");
     titTres.innerText="Fecha de nacimiento";
     inputCambio.appendChild(titTres);
